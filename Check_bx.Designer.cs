1ba9e1d28d024e9e088ee028830f1703987a6bcc
DavidLucas2021 <dlaredes@gmail.com>
2021-12-07 06:28:30 -0300

    Aplicação Garagem - Utilizando CheckBox

--- Check_bx.Designer.cs
+++ Check_bx.Designer.cs
@@ -29,21 +29,112 @@ namespace Estudo_do_Csharp_Windons_Forms_
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbx_1 = new System.Windows.Forms.CheckBox();
+            this.cbx2 = new System.Windows.Forms.CheckBox();
+            this.cbx3 = new System.Windows.Forms.CheckBox();
+            this.cbx4 = new System.Windows.Forms.CheckBox();
+            this.cbx5 = new System.Windows.Forms.CheckBox();
+            this.btn_Checkbox = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // cbx_1
+            // 
+            this.cbx_1.AutoSize = true;
+            this.cbx_1.Location = new System.Drawing.Point(28, 53);
+            this.cbx_1.Name = "cbx_1";
+            this.cbx_1.Size = new System.Drawing.Size(80, 17);
+            this.cbx_1.TabIndex = 0;
+            this.cbx_1.Text = "checkBox1";
+            this.cbx_1.UseVisualStyleBackColor = true;
+            // 
+            // cbx2
+            // 
+            this.cbx2.AutoSize = true;
+            this.cbx2.Location = new System.Drawing.Point(28, 76);
+            this.cbx2.Name = "cbx2";
+            this.cbx2.Size = new System.Drawing.Size(80, 17);
+            this.cbx2.TabIndex = 1;
+            this.cbx2.Text = "checkBox2";
+            this.cbx2.UseVisualStyleBackColor = true;
+            // 
+            // cbx3
+            // 
+            this.cbx3.AutoSize = true;
+            this.cbx3.Location = new System.Drawing.Point(28, 99);
+            this.cbx3.Name = "cbx3";
+            this.cbx3.Size = new System.Drawing.Size(80, 17);
+            this.cbx3.TabIndex = 2;
+            this.cbx3.Text = "checkBox3";
+            this.cbx3.UseVisualStyleBackColor = true;
+            // 
+            // cbx4
+            // 
+            this.cbx4.AutoSize = true;
+            this.cbx4.Location = new System.Drawing.Point(28, 122);
+            this.cbx4.Name = "cbx4";
+            this.cbx4.Size = new System.Drawing.Size(80, 17);
+            this.cbx4.TabIndex = 3;
+            this.cbx4.Text = "checkBox4";
+            this.cbx4.UseVisualStyleBackColor = true;
+            // 
+            // cbx5
+            // 
+            this.cbx5.AutoSize = true;
+            this.cbx5.Location = new System.Drawing.Point(28, 145);
+            this.cbx5.Name = "cbx5";
+            this.cbx5.Size = new System.Drawing.Size(80, 17);
+            this.cbx5.TabIndex = 4;
+            this.cbx5.Text = "checkBox5";
+            this.cbx5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Checkbox
+            // 
+            this.btn_Checkbox.Location = new System.Drawing.Point(159, 58);
+            this.btn_Checkbox.Name = "btn_Checkbox";
+            this.btn_Checkbox.Size = new System.Drawing.Size(112, 57);
+            this.btn_Checkbox.TabIndex = 5;
+            this.btn_Checkbox.Text = "Marcados";
+            this.btn_Checkbox.UseVisualStyleBackColor = true;
+            this.btn_Checkbox.Click += new System.EventHandler(this.btn_Checkbox_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 168);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 177);
+            this.textBox1.TabIndex = 6;
             // 
             // Check_bx
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 335);
+            this.ClientSize = new System.Drawing.Size(293, 351);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Checkbox);
+            this.Controls.Add(this.cbx5);
+            this.Controls.Add(this.cbx4);
+            this.Controls.Add(this.cbx3);
+            this.Controls.Add(this.cbx2);
+            this.Controls.Add(this.cbx_1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Check_bx";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CheckBox";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox cbx_1;
+        private System.Windows.Forms.CheckBox cbx2;
+        private System.Windows.Forms.CheckBox cbx3;
+        private System.Windows.Forms.CheckBox cbx4;
+        private System.Windows.Forms.CheckBox cbx5;
+        private System.Windows.Forms.Button btn_Checkbox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Check_bx.cs
+++ Check_bx.cs
@@ -12,9 +12,50 @@ namespace Estudo_do_Csharp_Windons_Forms_
 {
     public partial class Check_bx : Form
     {
+        List<CheckBox> list = new List<CheckBox>();
         public Check_bx()
         {
             InitializeComponent();
+            list.Add(cbx_1);
+            list.Add(cbx2);
+            list.Add(cbx3);
+            list.Add(cbx4);
+            list.Add(cbx5);
+        }
+
+        private void btn_Checkbox_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+            //LÓGICA DE CHECAGEM QUE DE CHECKBOX FEITO ELEMENTO POR ELEMENTO 
+            //if(cbx_1.checked)
+            //{
+            //    txt += cbx_1.text + environment.newline;
+            //}
+            //if (cbx2.checked)
+            //{
+            //    txt += cbx2.text + environment.newline;
+            //}
+            //if (cbx3.checked)
+            //{
+            //    txt += cbx3.text + environment.newline;
+            //}
+            //if (cbx4.checked)
+            //{
+            //    txt += cbx4.text + environment.newline;
+            //}
+            //if (cbx5.checked)
+            //{
+            //    txt += cbx5.text + environment.newline;
+            //}
+            //LÓGICA DE CHECAGEM FEITO COM FOREACH(MESMO EFEITO)
+            foreach(CheckBox lst in list)
+            {
+                if(lst.Checked)
+                {
+                    txt += lst.Text + Environment.NewLine;
+                }
+            }
+            textBox1.Text = txt;
         }
     }
 }
